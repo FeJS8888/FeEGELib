d445e185b02b0550b44ff8e66212f95409b25f5c
FeJS8888 <fmxfmx2007@163.com>
2025-05-12 12:06:53 +0800

    现在 readPolygonFromJSON 函数会自动处理顶点数据为顺时针

--- src/FeEGELib.h
+++ src/FeEGELib.h
@@ -1,8 +1,8 @@
 /**
  * @file FeEGELib.h
  * @brief FeEGE图形库主头文件
- * @version 2.0.10.1
- * @date 2025-05-10
+ * @version 2.0.10.2
+ * @date 2025-05-12
  * 
  * 这是一个基于 EGE 的图形开发框架，提供了元素管理、动画、事件处理等功能
  */
@@ -10,7 +10,7 @@
 #ifndef _FEEGELIB_
 #define _FEEGELIB_
 
-#define FeEGELib_version "V2.0.10.1--upd2025-05-10"  ///< 库版本号
+#define FeEGELib_version "V2.0.10.2--upd2025-05-12"  ///< 库版本号
 #define version() FeEGELib_version                   ///< 获取版本号的宏
 
 // 包含必要的头文件
