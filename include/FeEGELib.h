c5d07abe4133f0650314ed404af90a825617fad9
FeJS8888 <fmxfmx2007@163.com>
2025-11-14 19:46:19 +0800

    更新了版本号

--- include/FeEGELib.h
+++ include/FeEGELib.h
@@ -1,8 +1,8 @@
 ﻿/**
  * @file FeEGELib.h
  * @brief FeEGE图形库主头文件
- * @version 2.10.0.0
- * @date 2025-10-26
+ * @version 2.12.0.0
+ * @date 2025-11-14
  * 
  * 这是一个基于 EGE 的图形开发框架，提供了元素管理、动画、事件处理等功能
  */
@@ -10,7 +10,7 @@
 #ifndef _FEEGELIB_
 #define _FEEGELIB_
 
-#define FeEGELib_version "V2.10.0.0--upd2025-10-26"  ///< 库版本号
+#define FeEGELib_version "V2.12.0.0--upd2025-11-14"  ///< 库版本号
 #define version() FeEGELib_version                   ///< 获取版本号的宏
 
 #include "Base.h"      
